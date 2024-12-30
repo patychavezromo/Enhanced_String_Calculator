6c8854fdb5468493133a8aa1415d6ce23f542492
Paty Chavez <patricia.araceli.chavez.romo@gmail.com>
2024-12-30 15:13:44 -0600

    Refactor parse numbers

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,22 @@ import { useState } from 'react';
 import './App.css';
 import mathFunction from './mathFunction';
 
-
 function App() {
   const [inputValue, setInputValue] = useState('');
   const [result, setResult] = useState('');
 
   const processInput = () => {
     const stringNumbers = inputValue.trim();
-    if (stringNumbers === '') {
-      setResult(mathFunction.add([]));
-    }
-    const allNumbers = stringNumbers.split(',').map(stringNumber => parseInt(stringNumber)).filter(num => !isNaN(num));
-    setResult(mathFunction.add(allNumbers));
+    setResult(mathFunction.add(stringNumbers));
   };
 
   return (
     <>
       <h1>Enhanced String Calculator</h1>
-      <p> <b> Instructions: </b> Enter no number, one number, or two numbers separated by a comma:</p>
+      <p>
+        <b> Instructions: </b> Enter no number, one number, or two numbers
+        separated by a comma:
+      </p>
       <input
         id="input"
         type="text"
@@ -27,10 +25,10 @@ function App() {
         value={inputValue}
         onChange={(event) => setInputValue(event.target.value)}
       />
-      <button onClick={processInput} className="btn">Process data</button>
-      <div id="result">
-        Result: {result}
-      </div>
+      <button onClick={processInput} className="btn">
+        Process data
+      </button>
+      <div id="result">Result: {result}</div>
     </>
   );
 }
--- src/mathFunction.js
+++ src/mathFunction.js
@@ -1,15 +1,22 @@
-const mathFunction = {
-
-    add: (data) => {
-        if (data.length === 0) {
-            return 0;
-        }
-        if (data.length === 1) {
-            return data[0];
-        }
-        return data[0] + data[1];
-    }
-
+function parseNumbers(stringNumbers) {
+  const numbers = stringNumbers
+    .split(',')
+    .map((stringNumber) => parseInt(stringNumber))
+    .filter((num) => !isNaN(num));
+  return numbers;
 }
+
+const mathFunction = {
+  add: (stringNumbers) => {
+    if (stringNumbers === '') {
+      return 0;
+    }
+    const numbers = parseNumbers(stringNumbers);
+    if (numbers.length === 1) {
+      return numbers[0];
+    }
+    return numbers[0] + numbers[1];
+  },
+};
 
 export default mathFunction;
